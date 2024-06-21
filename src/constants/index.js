ccc98b625aa2699d2d24a9e318075bcda7e3cb41
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-06-21 13:38:15 +0100

    update abourt me section

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,11 @@ import couples from "../assets/projects/couples.jpg";
 
 export const HERO_CONTENT = `Hello, My name is Mike Wilkins and i am passionate about counselling and helping people to overcome any difficulties they may be having. I strongly believe that given the right conditions one can make significant changes in their lives. I offer a safe, non-judgemental and confidential environment where we can explore together any difficulties you may be experiencing.`;
 
-export const ABOUT_TEXT = `I have a BA Joint Honours degree in English literature, Sociology and Psychology. I have followed the CPCAB route to qualification by completing Level 2 in Counselling Skills, Level 3 in Counselling Studies and the Level 4 Diploma in Therapeutic Counselling. I also have an online diploma in e-counselling (online counselling). `;
+export const ABOUT_TEXT = `It’s my true belief that you know best on how to come through your own difficulties, and it is my job to help that happen. I believe that going for counselling should be the same as going to the GP if you have a medical problem or going to the gym to maintain your physical health and it is my mission to make counselling a normal, everyday thing.
+
+I have worked in a doctor’s surgery, a cancer support centre and at children and young persons' bereavement charity, so I have experienced a wide, cross section of people and their difficulties. I have worked in an Employee Assistance Programme, which helps support employees in the moment and I have developed skills to help people improve their situations by looking at their overall wellbeing in terms of improving their sleep, looking at ways of eating better, exercising more and all in a way that fits in with busy lifestyles.
+
+I have worked with depression, anxiety, anger management, bereavement, relationship issues, stress, suicide and life transition. I have particular expertise in working with men and youths and has a deep understanding of the difficulties that they face in modern day society. I also have extensive experience both professional and personal with Narcissist Personality Disorder and different ways how it can affect the person. `;
 
 export const PROJECTS = [
   {
